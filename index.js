5e6a3d473a2a7e3ae542dac0a2dfef051b35218f
adityakachade <harshalkachade04@gmail.com>
2024-11-30 21:22:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+let tasks = ["Task 1", "Task 2", "Task 3", "Task 4", "Task 5"];
+
+tasks.shift();
+tasks.unshift("High-priority Task 1", "High-priority Task 2");
+tasks[tasks.length - 1] = "New Last Task";
+
+console.log("Updated Task List:", tasks);
 
